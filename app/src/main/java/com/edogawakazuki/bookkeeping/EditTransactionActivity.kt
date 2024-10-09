ef18291762ce79090a9b7a696b5af09e93e7dbe2
EdogawaKazuki <galad.zhao@gmail.com>
2024-10-09 23:42:59 +0800

    - Added edit transaction page

--- app/src/main/java/com/edogawakazuki/bookkeeping/EditTransactionActivity.kt
+++ app/src/main/java/com/edogawakazuki/bookkeeping/EditTransactionActivity.kt
@@ -1,25 +1,51 @@
 package com.edogawakazuki.bookkeeping
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edogawakazuki.bookkeeping.ui.theme.BookkeepingTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+//Todo: Baidu IME not support the action.next
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 class EditTransactionActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,31 +58,127 @@ class EditTransactionActivity : ComponentActivity() {
                         TopAppBar(
                             title = { Text("Bookkeeping") },
                             actions = {
-                                Button(onClick = { /* enter activ */ }) {
-                                    Text("Try me")
-                                }
                             }
                         )
                     }
                 ) { innerPadding ->
-                    Column {
+                    val amount = remember { mutableStateOf("") }
+                    val description = remember { mutableStateOf("") }
+                    val date = remember { mutableStateOf("") }
+                    val category = remember { mutableStateOf("") }
+                    // auto focus switch
+                    val focusManager = LocalFocusManager.current
+                    val (focusRequester1, focusRequester2, focusRequester3, focusRequester4) = FocusRequester.createRefs()
+
+                    Column (
+                        modifier = Modifier.padding(innerPadding)
+                    ){
+                        LaunchedEffect(Unit) {
+                            focusRequester1.requestFocus()
+                        }
                         TextField(
-                            value = "",
-                            onValueChange = { },
+                            value = amount.value,
+                            onValueChange = { amount.value = it },
                             label = { Text("Amount") },
-                            modifier = Modifier.padding(innerPadding)
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .focusRequester(focusRequester1)
+                                .onFocusChanged { focusState ->
+                                    Log.d("Focus", "Amount TextField focused: ${focusState.isFocused}")
+                                },
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Next,
+                                keyboardType = KeyboardType.Number
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onNext = {
+                                    focusRequester2.requestFocus()
+                                    Log.d("EditTransactionActivity", "onNext")
+                                }
+                            )
                         )
+                        HorizontalDivider()
                         TextField(
-                            value = "",
-                            onValueChange = { },
+                            value = description.value,
+                            onValueChange = { description.value = it },
                             label = { Text("Description") },
-                            modifier = Modifier.padding(innerPadding)
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .focusRequester(focusRequester2)
+                                .onFocusChanged { focusState ->
+                                    Log.d("Focus", "Amount TextField focused: ${focusState.isFocused}")
+                                },
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Next
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onNext = { focusRequester3.requestFocus() }
+                            )
                         )
+                        HorizontalDivider()
+                        TextField(
+                            value = date.value,
+                            onValueChange = { date.value = it },
+                            label = { Text("Date") },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .focusRequester(focusRequester3)
+                                .onFocusChanged { focusState ->
+                                    Log.d("Focus", "Amount TextField focused: ${focusState.isFocused}")
+                                },
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Next
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onNext = { focusRequester4.requestFocus() }
+                            )
+                        )
+                        HorizontalDivider()
+                        TextField(
+                            value = category.value,
+                            onValueChange = { category.value = it },
+                            label = { Text("Category") },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .focusRequester(focusRequester4)
+                                .onFocusChanged { focusState ->
+                                    Log.d("Focus", "Amount TextField focused: ${focusState.isFocused}")
+                                },
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Done
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = { focusManager.clearFocus()
+                                    Log.d("EditTransactionActivity", "onDone")
+                                    submitTransaction(amount.value, description.value, date.value, category.value)
+                                }
+                            )
+                        )
+                        HorizontalDivider()
+                        Button(
+                            onClick = {
+                                submitTransaction(amount.value, description.value, date.value, category.value)
+                            },
+                            modifier = Modifier.align(Alignment.End)
+                        ) {
+                            Text("Submit")
+                        }
                     }
 
                 }
             }
         }
+    }
+
+    private fun submitTransaction(amount: String, description: String, date: String, category: String) {
+        val resultIntent = Intent().apply {
+            putExtra("amount", amount)
+            putExtra("description", description)
+            putExtra("date", date)
+            putExtra("category", category)
+        }
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
     }
 }
 
--- app/src/main/java/com/edogawakazuki/bookkeeping/MainActivity.kt
+++ app/src/main/java/com/edogawakazuki/bookkeeping/MainActivity.kt
@@ -1,32 +1,52 @@
 package com.edogawakazuki.bookkeeping
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Scroller
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edogawakazuki.bookkeeping.ui.theme.BookkeepingTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.math.log
+
+data class Transaction(
+    val amount: Double,
+    val description: String,
+    val date: String,
+    val category: String,
+)
+
+val transactions = mutableStateListOf(
+    Transaction(100.0, "Lunch", "2021-10-01", "Food"),
+    Transaction(200.0, "Dinner", "2021-10-02", "Food"),
+    Transaction(300.0, "Breakfast", "2021-10-03", "Food"),
+    Transaction(400.0, "Lunch", "2021-10-04", "Food"),
+    Transaction(500.0, "Dinner", "2021-10-05", "Food"),
+    Transaction(600.0, "Breakfast", "2021-10-06", "Food"),
+    Transaction(700.0, "Lunch", "2021-10-07", "Food"),
+)
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -52,7 +72,10 @@ class MainActivity : ComponentActivity() {
                     },
                     floatingActionButton = {
                         Column {
-                            FloatingActionButton (onClick = { CreateTransication() } ) {
+                            FloatingActionButton (onClick = {
+                                val intent = Intent(this@MainActivity, EditTransactionActivity::class.java)
+                                editTransactionLauncher.launch(intent)
+                            } ) {
                                 Text("+")
                             }
 
@@ -60,9 +83,9 @@ class MainActivity : ComponentActivity() {
                     },
                     content = { innerPadding ->
                         Box(
-                            modifier = Modifier.padding(innerPadding)
+                            modifier = Modifier
+                                .padding(innerPadding)
                                 .fillMaxSize(),
-                            contentAlignment = Alignment.Center,
                         ) {
                             Greeting(
                                 name = "Kazuki",
@@ -74,11 +97,21 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun CreateTransication() {
-        Toast.makeText(this, "Create Transaction", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, EditTransactionActivity::class.java)
-        startActivity(intent)
+    val editTransactionLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val amount = data?.getStringExtra("amount")
+            val description = data?.getStringExtra("description")
+            val date = data?.getStringExtra("date")
+            val category = data?.getStringExtra("category")
+            Toast.makeText(this, "Amount: $amount, Description: $description, Date: $date, Category: $category", Toast.LENGTH_SHORT).show()
+            transactions.add(Transaction(amount!!.toDouble(), description!!, date!!, category!!))
+        }
     }
+
 
     @Composable
     fun TabBar() {
@@ -121,8 +154,8 @@ class MainActivity : ComponentActivity() {
                     .weight(1f)
             ) { page ->
                 when (page) {
-                    0 -> HomeContent()
-                    1 -> ProfileContent()
+                    0 -> HomePage()
+                    1 -> TransactionPage()
                     2 -> SettingsContent()
                 }
             }
@@ -130,22 +163,34 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun HomeContent() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text("Home Screen")
+    fun HomePage() {
+    }
+
+    @Composable
+    fun TransactionItem(transaction: Transaction) {
+        Column {
+            Row {
+                Text(text = "Amount: ${transaction.amount}", modifier = Modifier.padding(end = 16.dp))
+                Text(text = "Date: ${transaction.date}", modifier = Modifier.padding(end = 16.dp))
+            }
+            Row{
+                Text(text = "Description: ${transaction.description}", modifier = Modifier.padding(end = 16.dp))
+                Text(text = "Category: ${transaction.category}", modifier = Modifier.padding(end = 16.dp))
+            }
+            HorizontalDivider()
         }
     }
 
     @Composable
-    fun ProfileContent() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+    fun TransactionPage() {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            Text("Profile Screen")
+            items(transactions) { transaction ->
+                TransactionItem(transaction)
+            }
         }
     }
 
